b75d63eb13855dfdb10d47f4dc8b88e4b2ff3c59
Hannah Mayer <hannah.mayer@outlook.at>
2020-05-01 18:22:11 +0200

    Angular Material part 3

--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -1,34 +1,45 @@
-import {Component, Inject, OnInit, ViewEncapsulation} from '@angular/core';
+import {
+  Component,
+  Inject,
+  OnInit,
+  ViewEncapsulation,
+  inject,
+} from "@angular/core";
 import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
-import {Course} from "../model/course";
-import {FormBuilder, Validators, FormGroup} from "@angular/forms";
-import * as moment from 'moment';
+import { Course } from "../model/course";
+import { FormBuilder, Validators, FormGroup, Form } from "@angular/forms";
+import * as moment from "moment";
 
 @Component({
-    selector: 'course-dialog',
-    templateUrl: './course-dialog.component.html',
-    styleUrls: ['./course-dialog.component.css']
+  selector: "course-dialog",
+  templateUrl: "./course-dialog.component.html",
+  styleUrls: ["./course-dialog.component.css"],
 })
 export class CourseDialogComponent implements OnInit {
+  description: string;
+  form: FormGroup;
 
+  constructor(
+    private fb: FormBuilder,
+    private dialogRef: MatDialogRef<CourseDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) { description, longDescription, category }: Course
+  ) {
+    this.description = description;
+    this.form = fb.group({
+      description: [description, Validators.required],
+      category: [category, Validators.required],
+      releasedAt: [moment(), Validators.required],
+      longDescription: [longDescription, Validators.required],
+    });
+  }
 
-    constructor() {
+  ngOnInit() {}
 
+  save() {
+    this.dialogRef.close(this.form.value);
+  }
 
-    }
-
-    ngOnInit() {
-
-    }
-
-
-    save() {
-
-
-    }
-
-    close() {
-
-    }
-
+  close() {
+    this.dialogRef.close();
+  }
 }
